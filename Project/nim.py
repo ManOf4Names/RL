3bc32895fee7d791c1c6a897805b4df2ee42326a
ThopClayton <60019875+ThopClayton@users.noreply.github.com>
2022-04-29 13:04:04 -0600

    Ramyaa preperations
    Fixed a bug where genAction was calling policy_template instead of policy. This change allows the genPolicy to be random before policy is set, but also to actually follow a non-random policy after the model is trained. This made genPolicyAction redundant so it was removed.
    
    Added stuff to main to make it play 1000 matches and print out win rate. Called before and after training.
    
    Minor bug fixes for human playable version.

--- Project/nim.py
+++ Project/nim.py
@@ -1,3 +1,4 @@
+# %%
 from policy import *
 import numpy as np
 import random
@@ -16,12 +17,12 @@ lose_reward = -50
 # episode length
 gameRunning = True
 
-
+# %%
 def Initialize():
     global policy
     global value
     global Q
-    policy = copy.deepcopy(Policy_Template)  # replaced .copy with a deepcopy
+    policy = copy.deepcopy(Policy_Template) #replaced .copy with a deepcopy
     value = copy.deepcopy(Value_Template)
     Q = copy.deepcopy(Q_Template)
 
@@ -34,11 +35,47 @@ def New_Game():
 
     return
 
+
+# %%
+"""
+A function for setting the policy for a given state
+    r1: Number of sticks in row 1
+    r2: Number of sticks in row 2
+    r3: Number of sticks in row 3
+    r4: Number of sticks in row 4
+    count: Total number of sticks left
+"""
+def setPolicy(r1, r2, r3, r4, count):
+    policy = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    for i in range(r1):
+        policy[0 + i] = 1.0/count
+    for i in range(r2):
+        policy[1 + i] = 1.0/count
+    for i in range(r3):
+        policy[4 + i] = 1.0/count
+    for i in range(r4):
+        policy[9 + i] = 1.0/count
+    return policy
+
+"""
+Generates a new policy for NIM
+"""
+def newPolicy():
+    for r1 in range(2):
+        for r2 in range(4):
+            for r3 in range(6):
+                for r4 in range(8):
+                    print((r1, r2, r3, r4), end='')
+                    print(" : ", end='')
+                    print(setPolicy(r1, r2, r3, r4, r1 + r2 + r3 + r4), end='')
+                    print(",")
+
+# %%
 """
 Observes the current state space, and makes a random legal action
 """
 def genRandomAction(state):
-    while True:  # it randomly chooses actions until it finds one that is legal
+    while True: # it randomly chooses actions until it finds one that is legal
         action = random.randint(0, 15)
         if Policy_Template[tuple(state)][action] != 0:
             break
@@ -53,13 +90,15 @@ def genAction(state):
     actionChance = random.uniform(0, 1)
     action = 0
     sum = 0
-    for x in Policy_Template[state]:
+    #for x in Policy_Template[state]:
+    for x in policy[state]:
         sum += x
-        if sum > actionChance:
+        if sum > actionChance: 
             break
         else:
             action += 1
     return Actions[action]
+
 
 
 """
@@ -69,12 +108,13 @@ If it needs to remove more sticks than what the row contains, it terminates the 
 def Env(a, state):
     row, nSticks = a
     new_state = list(state)
-    if new_state[row - 1] < nSticks:  # simple error check
+    if new_state[row - 1] < nSticks: # simple error check
         print("Attempting to remove more sticks than possible, this shouldn't be possible!")
         quit()
     else:
-        new_state[row - 1] -= nSticks  # removes specified stick #
+        new_state[row - 1] -= nSticks # removes specified stick #
 
+    # print(new_state)
     if (tuple(new_state) == win):
         gameRunning == False
         return (tuple(new_state), win_reward)
@@ -92,6 +132,21 @@ def Env(a, state):
     else:
         return (tuple(new_state), 0)
 
+
+"""
+Plays the game by taking random legal moves
+
+Depreciated :(
+"""
+def genEpisodeRandomly():
+    New_Game()
+    print(state)
+    while (tuple(state) != win):
+        genRandomAction()
+        print("action taken")
+        print(state)
+
+# %%
 """
 play the game for on episode:
     return States, Actions_choses, Reward, Returns
@@ -135,7 +190,6 @@ def Gen_Episode():
         # Increment the time variable
         t += 1
 
-
     # After intial loop, set initial return for last step
     Return[-1] = Reward[-1]
     # Then iterate backwards and assign rewards for each stage.
@@ -143,9 +197,72 @@ def Gen_Episode():
         Return[r] = Reward[r] + Gamma * Return[r + 1]
     return (States, Actions_chosen, Reward, Return)
 
+"""
+Helper function that takes in an action tuple and returns what it's index in the Actions dict would be
+"""
+def actionIndex(tuple):
+    for i in range(16):
+        if Actions[i] == tuple:
+            return i
 
+
+"""
+For each state, for the the action taken, the corresponding Q value has the reward for that state/action combo added
+"""
+
+def MC_Q_Evaluate():
+    global Q
+    (States, Actions_chosen, Reward, Return) = Gen_Episode()
+    for state in States:
+        if state != (0, 0, 0, 0):  # added condition since Q value does not consider end state
+            Q[state][actionIndex(Actions_chosen[States.index( # Actions_chosen[States.index(state)] was giving tuple when int was needed. Added helper function "actionIndex" to create desired functionality
+                state)])] += Return[States.index(state)] # replace Reward[States.index(state)] with Return[States.index(state)]
+        #Q[r][c][Actions.index(a)] += Return[States.index((r, c))] <- original midterm code!
+
+"""
+Examines the Q table for each state and action pair. The action with the highest score for a given state will set the policy.
+"""
+def MC_find_policy(maxiter):
+    for w in range(maxiter):
+        MC_Q_Evaluate()
+
+
+    for index in Q:
+        # initializes policy to first legal action. Needed for situations where only possible move results in loss
+        optimal = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        for i in range(16):
+            if Q[index][i] != 0:
+                max = Q[index][i]
+                maxIndex = i
+                break
+
+
+        for i in range(16):
+            if Q[index][i] > max and Q[index][i] != 0:
+                max = Q[index][i]
+                maxIndex = i
+        optimal[maxIndex] = 1
+        policy[index] = optimal     
+
+def policyPlay(maxiter):
+    wins = 0
+    losses = 0
+    for i in range(maxiter):
+        s, a, rew, ret = Gen_Episode()
+        if ret[0] < 0:
+            losses += 1
+        else:
+            wins += 1
+    print("with {} matches, the RL AI has {} wins and {} losses".format(maxiter, wins, losses))
+    print("Win rate: {}%".format(wins/maxiter * 100))
+
+
+
+# %%
 def main():
     Initialize()
+
+    #example run below
     s, a, rew, ret = Gen_Episode()
     for i in range(len(s)):
         print(f"Time: {i}")
@@ -153,7 +270,109 @@ def main():
         print(f"Action: {a[i]}")
         print(f"Reward: {rew[i]}")
         print(f"Return: {ret[i]}\n")
+        
+    # garrett testing (he should delete this part before uploading to git)
+    #MC_Q_Evaluate()
+    policyPlay(1000)
 
+    MC_find_policy(1000000)
+    
+    #print out Q table and Policy for each state
+    if False:
+        for x in policy:
+            print(x)
+            print(Q[x])
+            print(policy[x])
+
+    #actual run after policy found
+    print("training completed")
+    policyPlay(1000)
 
 if __name__ == "__main__":
     main()
+
+
+# %%
+import sys
+
+"""
+Takes in user input in form "row sticks" and does it. Has some basic error checking
+"""
+def playerMove(state):
+    while True:
+        print("Enter your move in form 'row sticks' or 'quit'\n>>> ", end='')
+        sys.stdout.flush()
+
+        inp = input()
+        sys.stdout.flush() # strategically placed flush
+
+        #print(inp)
+        if inp == "quit":
+            quit()
+
+        action = list(map(int, inp.split()))
+        print()
+
+        row = action[0] # there is totally a better way to do this, but it is 11:45pm
+        nSticks = action[1]
+
+
+
+        if row > 5 or row <= 0:
+            print("Ensure the row number is either 1, 2, 3 or 4")
+
+        elif state[row - 1] < nSticks: # simple error check
+            print("Attempting to remove more sticks than possible!")
+
+        else:
+            state[row - 1] -= nSticks
+            break
+"""
+Sort of like Env but only returns the new state. Takes in action tuple and state tuple
+"""
+def playableEnv(a, state): # ugly hack but whatever it's 12:08am
+    row, nSticks = a
+    new_state = list(state)
+    if new_state[row - 1] < nSticks: # simple error check
+        print("Attempting to remove more sticks than possible, this shouldn't be possible!")
+        quit()
+    else:
+        new_state[row - 1] -= nSticks # removes specified stick #
+
+    return tuple(new_state)
+
+
+
+def playNim():
+    state = copy.deepcopy(newGame)
+    print("New state:")
+    print(state)
+    while True:
+        #AI doing its thing
+        state = list(playableEnv(genAction(tuple(state)), tuple(state)))
+        print("State after AI move:")
+        print(state)
+        if state == [0, 0, 0, 0]:
+            print("AI wins!")
+            break
+
+        #player playing (12:50am fuck you bed time)
+        playerMove(state)
+        print("State after your move:")
+        print(state)
+        if state == [0, 0, 0, 0]:
+            print("user wins!")
+            break
+
+def playNimForever():
+    while True:
+        playNim()
+        sys.stdout.flush() # strategically placed flush
+
+
+
+
+
+playNimForever()
+
+
